76a0dc9867d11d4d66aa8ae71454a6101b530d04
Himanshu Sharma <himanshush214@gmail.com>
2020-04-10 03:07:42 +0530

    Updates

---
+++ precache-manifest.218f48e083b115f763cf95d32b6729bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6b7d0ec9d90b7cc59a7",
+    "url": "/MohitEditor/static/css/main.7ba37fe1.chunk.css"
+  },
+  {
+    "revision": "f6b7d0ec9d90b7cc59a7",
+    "url": "/MohitEditor/static/js/main.f6b7d0ec.chunk.js"
+  },
+  {
+    "revision": "6221052083d9d7c06dcf",
+    "url": "/MohitEditor/static/js/1.62210520.chunk.js"
+  },
+  {
+    "revision": "94e84245666e3aed9cb5",
+    "url": "/MohitEditor/static/js/runtime~main.94e84245.js"
+  },
+  {
+    "revision": "902ae4afe339754b178f4fd1961f8dec",
+    "url": "/MohitEditor/index.html"
+  }
+];
